cb4f2e126b0a83993b5c580f02b555b038dac43a
ALUNO <renanmcardozo@yahoo.com.br>
2024-08-28 20:29:28 -0300

    Atualização depois de arrumar o Migration

--- Migrations/20240828231435_inicio.cs
+++ Migrations/20240828231435_inicio.cs
@@ -5,7 +5,7 @@
 namespace Piadas.Migrations
 {
     /// <inheritdoc />
-    public partial class Iniciodaaplicação : Migration
+    public partial class inicio : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
